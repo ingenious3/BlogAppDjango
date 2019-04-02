f444c2df2d3e13531b1bbe893aa2dd01b0f805b7
ingenious3 <ishant.7123217@gmail.com>
2019-04-02 16:46:51 +0530

    add, edit, delete post

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Post(models.Model):
 
@@ -9,3 +9,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
